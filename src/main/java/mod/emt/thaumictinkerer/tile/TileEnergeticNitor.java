1166a6b78b812899b046b5026176f1eeceb03106
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-03-26 06:49:51 -0500

    Add energetic nitor particles

--- src/main/java/mod/emt/thaumictinkerer/tile/TileEnergeticNitor.java
+++ src/main/java/mod/emt/thaumictinkerer/tile/TileEnergeticNitor.java
@@ -2,8 +2,10 @@ package mod.emt.thaumictinkerer.tile;
 
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
+import thaumcraft.client.fx.FXDispatcher;
 
 public class TileEnergeticNitor extends TileEntity implements ITickable {
+    public int count = 0;
     //TODO: Configurable duration?
     public int duration = 20;
 
@@ -14,6 +16,14 @@ public class TileEnergeticNitor extends TileEntity implements ITickable {
                 this.duration--;
             } else {
                 this.world.setBlockToAir(this.pos);
+            }
+        }
+
+        if(world.isRemote) {
+            FXDispatcher.INSTANCE.drawNitorFlames(pos.getX() + 0.5F + world.rand.nextGaussian() * 0.025D, pos.getY() + 0.45F + world.rand.nextGaussian() * 0.025D,
+                    pos.getZ() + 0.5F + world.rand.nextGaussian() * 0.025D, world.rand.nextGaussian() * 0.0025D, world.rand.nextFloat() * 0.06F, world.rand.nextGaussian() * 0.0025D, 15606528, 0);
+            if(count++ % 10 == 0) {
+                FXDispatcher.INSTANCE.drawNitorCore(pos.getX() + 0.5F, pos.getY() + 0.49F, pos.getZ() + 0.5F, 0.0, 0.0, 0.0);
             }
         }
     }
