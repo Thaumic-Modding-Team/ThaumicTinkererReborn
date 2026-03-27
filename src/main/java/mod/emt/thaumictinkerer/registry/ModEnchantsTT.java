49ea9cdc631d2b792700943d507e34e12120fd51
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-03-27 00:09:21 -0500

    Tweak list

--- src/main/java/mod/emt/thaumictinkerer/registry/ModEnchantsTT.java
+++ src/main/java/mod/emt/thaumictinkerer/registry/ModEnchantsTT.java
@@ -23,10 +23,7 @@ public class ModEnchantsTT {
     sandstone
     stone
     stoneAndesite
-    stoneAndesitePolished
     stoneDiorite
-    stoneDioritePolished
     stoneGranite
-    stoneGranitePolished
      */
 }
