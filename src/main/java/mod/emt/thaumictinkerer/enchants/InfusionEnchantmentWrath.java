9530e34af8667d9b0ba4d1c2b06296f971571bbc
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-04-05 17:49:35 -0500

    Update wrath event

--- src/main/java/mod/emt/thaumictinkerer/enchants/InfusionEnchantmentWrath.java
+++ src/main/java/mod/emt/thaumictinkerer/enchants/InfusionEnchantmentWrath.java
@@ -1,39 +1,36 @@
 package mod.emt.thaumictinkerer.enchants;
 
+import com.expandedevents.events.ItemAttributeModifierEvent;
 import com.invadermonky.thaumicapi.api.ThaumicAPI;
 import mod.emt.thaumictinkerer.api.IAddition;
 import mod.emt.thaumictinkerer.api.IProxy;
 import mod.emt.thaumictinkerer.config.ConfigHandlerTT;
 import mod.emt.thaumictinkerer.registry.ModEnchantsTT;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.util.DamageSource;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 import thaumcraft.common.lib.enchantment.EnumInfusionEnchantment;
 
+import java.util.UUID;
+
 // TODO: Temporary implementation until we adjust the attribute directly
 @Mod.EventBusSubscriber
 public class InfusionEnchantmentWrath implements IAddition, IProxy {
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void onLivingHurt(LivingHurtEvent event) {
-        DamageSource damageSource = event.getSource();
-        Entity trueSource = damageSource.getTrueSource();
+    @SubscribeEvent
+    public static void onItemAttribute(ItemAttributeModifierEvent event) {
+        ItemStack stack = event.getItemStack();
+        int level = EnumInfusionEnchantment.getInfusionEnchantmentLevel(stack, ModEnchantsTT.WRATH);
+        double bonus = 0.5D + (0.5D * level);
 
-        if(trueSource instanceof EntityPlayer && trueSource != null) {
-            ItemStack heldStack = ((EntityPlayer) trueSource).getHeldItemMainhand();
-            int level = EnumInfusionEnchantment.getInfusionEnchantmentLevel(heldStack, ModEnchantsTT.WRATH);
-
-            double bonus = 0.5D + (0.5D * level);
-            if(level > 0) {
-                event.setAmount(event.getAmount() + (float) bonus);
-            }
+        if(level > 0 && event.getSlotType() == EntityEquipmentSlot.MAINHAND) {
+            event.addModifier(SharedMonsterAttributes.ATTACK_DAMAGE, new AttributeModifier(UUID.fromString("B81707CB-A9BB-45E3-B656-B8E48BFC8CF8"), "Weapon modifier", bonus, Constants.AttributeModifierOperation.ADD));
         }
     }
 
