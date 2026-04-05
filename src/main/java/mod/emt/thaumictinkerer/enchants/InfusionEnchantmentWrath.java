207f088ea1049050d413c53aea335395c410ac8a
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-04-05 18:05:50 -0500

    Update wrath event again

--- src/main/java/mod/emt/thaumictinkerer/enchants/InfusionEnchantmentWrath.java
+++ src/main/java/mod/emt/thaumictinkerer/enchants/InfusionEnchantmentWrath.java
@@ -12,25 +12,45 @@ import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 import thaumcraft.common.lib.enchantment.EnumInfusionEnchantment;
 
+import java.util.Collection;
 import java.util.UUID;
 
 // TODO: Temporary implementation until we adjust the attribute directly
 @Mod.EventBusSubscriber
 public class InfusionEnchantmentWrath implements IAddition, IProxy {
+    //Copy of `Item#ATTACK_DAMAGE_MODIFIER`
+    public static final UUID ATTACK_DAMAGE_MODIFIER = UUID.fromString("CB3F55D3-645C-4F38-A497-9C13A33DB5CF");
+
     @SubscribeEvent
     public static void onItemAttribute(ItemAttributeModifierEvent event) {
         ItemStack stack = event.getItemStack();
         int level = EnumInfusionEnchantment.getInfusionEnchantmentLevel(stack, ModEnchantsTT.WRATH);
         double bonus = 0.5D + (0.5D * level);
 
-        if(level > 0 && event.getSlotType() == EntityEquipmentSlot.MAINHAND) {
-            event.addModifier(SharedMonsterAttributes.ATTACK_DAMAGE, new AttributeModifier(UUID.fromString("B81707CB-A9BB-45E3-B656-B8E48BFC8CF8"), "Weapon modifier", bonus, Constants.AttributeModifierOperation.ADD));
+        if(level > 0  && event.getSlotType() == EntityEquipmentSlot.MAINHAND) {
+            Collection<AttributeModifier> modifiers = event.getOriginalModifiers().get(SharedMonsterAttributes.ATTACK_DAMAGE.getName());
+            AttributeModifier toModify = null;
+            for(AttributeModifier modifier : modifiers) {
+                if(modifier.getID().equals(ATTACK_DAMAGE_MODIFIER)) {
+                    toModify = modifier;
+                    break;
+                }
+            }
+
+            if(toModify != null) {
+                event.removeModifier(SharedMonsterAttributes.ATTACK_DAMAGE, toModify);
+                event.addModifier(SharedMonsterAttributes.ATTACK_DAMAGE, new AttributeModifier(
+                        toModify.getID(),
+                        toModify.getName(),
+                        toModify.getAmount() + bonus,
+                        toModify.getOperation())
+                );
+            }
         }
     }
 
