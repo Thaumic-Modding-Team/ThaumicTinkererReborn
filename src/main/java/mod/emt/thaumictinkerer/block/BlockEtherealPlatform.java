beea065c55a77df6443bca4c20f2ce97fe1ab4de
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2026-04-04 23:16:44 -0700

    stupid platform...

--- src/main/java/mod/emt/thaumictinkerer/block/BlockEtherealPlatform.java
+++ src/main/java/mod/emt/thaumictinkerer/block/BlockEtherealPlatform.java
@@ -1,52 +1,34 @@
 package mod.emt.thaumictinkerer.block;
 
-import mod.emt.thaumictinkerer.api.block.BlockTileAddition;
-import mod.emt.thaumictinkerer.tile.TileEtherealPlatform;
+import mod.emt.thaumictinkerer.api.block.AbstractBlockAddition;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyBool;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class BlockEtherealPlatform extends BlockTileAddition {
-    public static final PropertyBool ETHEREAL = PropertyBool.create("ethereal");
-    public static final AxisAlignedBB EMPTY_AABB = new AxisAlignedBB(0, 0, 0, 0, 0, 0);
-
+public class BlockEtherealPlatform extends AbstractBlockAddition {
     public BlockEtherealPlatform() {
-        super("ethereal_platform", Material.ROCK, MapColor.STONE, TileEtherealPlatform.class);
+        super("ethereal_platform", Material.ROCK, MapColor.STONE);
         this.setHardness(2.0f);
         this.setResistance(12.0f);
         this.setSoundType(SoundType.STONE);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(ETHEREAL, false));
     }
 
     @SuppressWarnings("deprecation")
     @Override
     public void addCollisionBoxToList(@NotNull IBlockState state, @NotNull World worldIn, @NotNull BlockPos pos, @NotNull AxisAlignedBB entityBox, @NotNull List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean isActualState) {
-        if(entityIn instanceof EntityPlayer && state.getValue(ETHEREAL)) {
-            return;
+        if (entityIn != null && entityIn.posY > (pos.getY() + (entityIn instanceof EntityPlayer ? 0.9 : 0)) && (!(entityIn instanceof EntityPlayer) || !entityIn.isSneaking())) {
+            super.addCollisionBoxToList(state, worldIn, pos, entityBox, collidingBoxes, entityIn, isActualState);
         }
-        super.addCollisionBoxToList(state, worldIn, pos, entityBox, collidingBoxes, entityIn, isActualState);
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    public @Nullable AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, @NotNull IBlockAccess worldIn, @NotNull BlockPos pos) {
-        return blockState.getValue(ETHEREAL) ? EMPTY_AABB : FULL_BLOCK_AABB;
     }
 
     @SuppressWarnings("deprecation")
@@ -65,27 +47,5 @@ public class BlockEtherealPlatform extends BlockTileAddition {
     @Override
     public boolean causesSuffocation(@NotNull IBlockState state) {
         return false;
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    public @NotNull IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(ETHEREAL, meta == 1);
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(ETHEREAL) ? 1 : 0;
-    }
-
-    @Override
-    protected @NotNull BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, ETHEREAL);
-    }
-
-    @Override
-    public void registerModel(ModelRegistryEvent event) {
-        super.registerModel(event);
-        ModelLoader.setCustomStateMapper(this, new StateMap.Builder().ignore(ETHEREAL).build());
     }
 }
