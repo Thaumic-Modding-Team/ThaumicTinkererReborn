827e79f2d9f895f90041f34d07e64d811a30896d
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2026-04-05 01:02:39 -0700

    modify passable wall logic

--- src/main/java/mod/emt/thaumictinkerer/block/BlockPassableWall.java
+++ src/main/java/mod/emt/thaumictinkerer/block/BlockPassableWall.java
@@ -2,18 +2,24 @@ package mod.emt.thaumictinkerer.block;
 
 import mod.emt.thaumictinkerer.api.block.BlockTileAddition;
 import mod.emt.thaumictinkerer.tile.TilePassableWall;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,6 +27,7 @@ import java.util.List;
 
 public class BlockPassableWall extends BlockTileAddition {
     public static final AxisAlignedBB EMPTY_AABB = new AxisAlignedBB(0, 0, 0, 0, 0, 0);
+    public static final PropertyInteger LEVEL = BlockLiquid.LEVEL;
     public static final PropertyBool PASSABLE = PropertyBool.create("passable");
 
     public BlockPassableWall() {
@@ -28,13 +35,19 @@ public class BlockPassableWall extends BlockTileAddition {
         this.setHardness(3.0f);
         this.setResistance(20.0f);
         this.setSoundType(SoundType.STONE);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(PASSABLE, false));
+        this.setDefaultState(this.blockState.getBaseState().withProperty(PASSABLE, false).withProperty(LEVEL, 15));
     }
 
     @SuppressWarnings("deprecation")
     @Override
     public @NotNull AxisAlignedBB getBoundingBox(IBlockState state, @NotNull IBlockAccess source, @NotNull BlockPos pos) {
         return state.getValue(PASSABLE) ? EMPTY_AABB : super.getBoundingBox(state, source, pos);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public @Nullable AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, @NotNull IBlockAccess worldIn, @NotNull BlockPos pos) {
+        return blockState.getValue(PASSABLE) ? NULL_AABB : FULL_BLOCK_AABB;
     }
 
     @SuppressWarnings("deprecation")
@@ -57,9 +70,10 @@ public class BlockPassableWall extends BlockTileAddition {
         }
     }
 
+    @SuppressWarnings("deprecation")
     @Override
-    public @Nullable Boolean isEntityInsideMaterial(@NotNull IBlockAccess world, @NotNull BlockPos blockpos, IBlockState state, @NotNull Entity entity, double yToTest, @NotNull Material materialIn, boolean testingHead) {
-        return !state.getValue(PASSABLE);
+    public @NotNull Material getMaterial(IBlockState state) {
+        return state.getValue(PASSABLE) ? Material.WATER : super.getMaterial(state);
     }
 
     @SuppressWarnings("deprecation")
@@ -78,6 +92,12 @@ public class BlockPassableWall extends BlockTileAddition {
     @Override
     public boolean causesSuffocation(IBlockState state) {
         return !state.getValue(PASSABLE);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean shouldSideBeRendered(@NotNull IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, @NotNull EnumFacing side) {
+        return blockAccess.getBlockState(pos.offset(side)).getBlock() != this && (side == EnumFacing.UP || super.shouldSideBeRendered(blockState, blockAccess, pos, side));
     }
 
     @Override
@@ -106,6 +126,12 @@ public class BlockPassableWall extends BlockTileAddition {
 
     @Override
     protected @NotNull BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, PASSABLE);
+        return new BlockStateContainer(this, PASSABLE, LEVEL);
+    }
+
+    @Override
+    public void registerModel(ModelRegistryEvent event) {
+        super.registerModel(event);
+        ModelLoader.setCustomStateMapper(this, new StateMap.Builder().ignore(LEVEL).build());
     }
 }
