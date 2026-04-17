9e320049654074d394059d0899aaf2313bc10f6b
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-04-16 19:07:16 -0500

    Setup research tab and first research

--- src/main/java/mod/emt/thaumictinkerer/proxy/CommonProxy.java
+++ src/main/java/mod/emt/thaumictinkerer/proxy/CommonProxy.java
@@ -1,11 +1,17 @@
 package mod.emt.thaumictinkerer.proxy;
 
+import mod.emt.thaumictinkerer.ThaumicTinkerer;
 import mod.emt.thaumictinkerer.api.IAddition;
 import mod.emt.thaumictinkerer.api.IProxy;
 import mod.emt.thaumictinkerer.compat.crafttweaker.CTPlugin;
 import mod.emt.thaumictinkerer.registry.RegistrarTT;
 import mod.emt.thaumictinkerer.utils.helpers.CompatHelper;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import thaumcraft.Thaumcraft;
+import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.aspects.AspectList;
+import thaumcraft.api.research.ResearchCategories;
 
 public class CommonProxy {
     public void preInit() {
@@ -26,6 +32,13 @@ public class CommonProxy {
 
     private void registerResearch() {
         //TODO: Register research.
+        ResearchCategories.registerCategory(
+                "THAUMIC_TINKERER", "FIRSTSTEPS", new AspectList(),
+                new ResourceLocation(ThaumicTinkerer.MOD_ID, "textures/research/r_thaumic_tinkering.png"),
+                new ResourceLocation(ThaumicTinkerer.MOD_ID, "textures/gui/research_background.png"),
+                new ResourceLocation(Thaumcraft.MODID, "textures/gui/gui_research_back_over.png"));
+
+        ThaumcraftApi.registerResearchLocation(new ResourceLocation(ThaumicTinkerer.MOD_ID, "research/basics"));
         RegistrarTT.getAdditions().forEach(IAddition::registerResearchLocation);
     }
 }
