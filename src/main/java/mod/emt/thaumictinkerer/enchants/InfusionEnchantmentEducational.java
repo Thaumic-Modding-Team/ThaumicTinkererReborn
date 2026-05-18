bee62ce69b800f206aff6e2b8761987bda9cff3f
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2026-05-18 15:04:32 -0700

    add block break xp bonus

--- src/main/java/mod/emt/thaumictinkerer/enchants/InfusionEnchantmentEducational.java
+++ src/main/java/mod/emt/thaumictinkerer/enchants/InfusionEnchantmentEducational.java
@@ -10,6 +10,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import thaumcraft.common.lib.enchantment.EnumInfusionEnchantment;
@@ -30,8 +31,25 @@ public class InfusionEnchantmentEducational implements IAddition, IProxy {
                 if(cap > 0 && bonus > cap) {
                     bonus = cap;
                 }
-
                 event.setDroppedExperience(experience + (int) Math.round(bonus));
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onBlockBreak(BlockEvent.BreakEvent event) {
+        EntityPlayer player = event.getPlayer();
+        if(player != null) {
+            ItemStack heldStack = player.getHeldItemMainhand();
+            int level = EnumInfusionEnchantment.getInfusionEnchantmentLevel(heldStack, ModEnchantsTT.EDUCATIONAL);
+            if(level > 0) {
+                int experience = event.getExpToDrop();
+                double bonus = experience * (ConfigHandlerTT.infusionEnchantments.educational.xpGainPerLevel * level);
+                int cap = ConfigHandlerTT.infusionEnchantments.educational.maxXpPerLevel * level;
+                if(cap > 0 && bonus > cap) {
+                    bonus = cap;
+                }
+                event.setExpToDrop(experience + (int) Math.round(bonus));
             }
         }
     }
