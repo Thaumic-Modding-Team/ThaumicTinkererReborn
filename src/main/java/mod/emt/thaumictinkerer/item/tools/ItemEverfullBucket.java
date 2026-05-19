124c76b4d02f0ea84bad57036e98e991c6369c7b
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2026-05-19 15:12:28 -0700

    overhaul bucket

--- src/main/java/mod/emt/thaumictinkerer/item/tools/ItemEverfullBucket.java
+++ src/main/java/mod/emt/thaumictinkerer/item/tools/ItemEverfullBucket.java
@@ -1,37 +1,93 @@
 package mod.emt.thaumictinkerer.item.tools;
 
-import mod.emt.thaumictinkerer.ThaumicTinkerer;
-import mod.emt.thaumictinkerer.api.item.IItemAddition;
-import net.minecraft.advancements.CriteriaTriggers;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
+import mod.emt.thaumictinkerer.api.item.AbstractItemAddition;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.EnumRarity;
-import net.minecraft.item.ItemBucket;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stats.StatList;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagInt;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IRarity;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.fluids.*;
+import net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import thaumcraft.api.aura.AuraHelper;
 import thaumcraft.client.fx.FXDispatcher;
 
-import javax.annotation.Nullable;
-import java.util.Objects;
+public class ItemEverfullBucket extends AbstractItemAddition {
+    private static final int MAX_QUEUE = 10;
+    private static final FluidStack WATER_STACK = new FluidStack(FluidRegistry.WATER, Fluid.BUCKET_VOLUME);
 
-public class ItemEverfullBucket extends ItemBucket implements IItemAddition {
     public ItemEverfullBucket() {
-        super(Blocks.FLOWING_WATER);
-        this.setRegistryName(ThaumicTinkerer.MOD_ID, "everfull_bucket");
-        this.setTranslationKey(Objects.requireNonNull(this.getRegistryName()).toString());
-        this.setCreativeTab(ThaumicTinkerer.tabTT);
+        super("everfull_bucket");
+        this.setMaxStackSize(1);
+    }
+
+    @Override
+    public @Nullable ICapabilityProvider initCapabilities(@NotNull ItemStack stack, @Nullable NBTTagCompound nbt) {
+        return new EverfullBucketWrapper(stack);
+    }
+
+    @Override
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+        return oldStack.getItem() != newStack.getItem() || slotChanged;
+    }
+
+    @Override
+    public void onUpdate(@NotNull ItemStack stack, @NotNull World worldIn, @NotNull Entity entityIn, int itemSlot, boolean isSelected) {
+        if(!worldIn.isRemote) {
+            int queue = getQueue(stack);
+            if(queue > 0 && AuraHelper.getVis(worldIn, entityIn.getPosition()) >= 2.0f) {
+                AuraHelper.drainVis(worldIn, entityIn.getPosition(), 2.0f, false);
+                setQueue(stack, queue - 1);
+            }
+        }
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public @NotNull ActionResult<ItemStack> onItemRightClick(@NotNull World worldIn, @NotNull EntityPlayer playerIn, @NotNull EnumHand handIn) {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+        RayTraceResult trace = this.rayTrace(worldIn, playerIn, false);
+        ActionResult<ItemStack> result = ForgeEventFactory.onBucketUse(playerIn, worldIn, stack, trace);
+        if (result != null) {
+            return result;
+        }
+
+        if(trace != null && trace.typeOfHit == RayTraceResult.Type.BLOCK) {
+            BlockPos tracePos = trace.getBlockPos();
+            if (worldIn.isBlockModifiable(playerIn, tracePos)) {
+                boolean isReplaceable = worldIn.getBlockState(tracePos).getBlock().isReplaceable(worldIn, tracePos);
+                BlockPos offset = isReplaceable && trace.sideHit == EnumFacing.UP ? tracePos : tracePos.offset(trace.sideHit);
+                if (playerIn.canPlayerEdit(offset, trace.sideHit, stack)) {
+                    FluidActionResult fluidAction = tryPlaceContainedLiquid(playerIn, stack, worldIn, offset);
+                    if (fluidAction.isSuccess()) {
+                        return new ActionResult<>(EnumActionResult.SUCCESS, fluidAction.getResult());
+                    }
+                }
+            }
+            return new ActionResult<>(EnumActionResult.FAIL, stack);
+        }
+        return new ActionResult<>(EnumActionResult.PASS, stack);
+    }
+
+    public FluidActionResult tryPlaceContainedLiquid(@Nullable EntityPlayer player, ItemStack stack, World world, BlockPos pos) {
+        FluidActionResult result = FluidUtil.tryPlaceFluid(player, world, pos, stack, WATER_STACK);
+        if(result.isSuccess()) {
+            if(world.isRemote) {
+                for(int k = 0; k < 20; ++k) {
+                    FXDispatcher.INSTANCE.jarSplashFx(pos.getX() + 0.5D, pos.getY() + 1.0D, pos.getZ() + 0.5D);
+                }
+            }
+        }
+        return result;
     }
 
     @Override
@@ -39,74 +95,60 @@ public class ItemEverfullBucket extends ItemBucket implements IItemAddition {
         return EnumRarity.RARE;
     }
 
-    @Override
-    public @NotNull ActionResult<ItemStack> onItemRightClick(@NotNull World world, @NotNull EntityPlayer player, @NotNull EnumHand hand) {
-        ItemStack stack = player.getHeldItem(hand);
-        RayTraceResult raytraceresult = rayTrace(world, player, false);
-        ActionResult<ItemStack> result = ForgeEventFactory.onBucketUse(player, world, stack, raytraceresult);
-
-        if(result != null) {
-            return result;
-        }
-
-        if(raytraceresult == null) {
-            return new ActionResult<>(EnumActionResult.PASS, stack);
-        } else if (raytraceresult.typeOfHit != RayTraceResult.Type.BLOCK) {
-            return new ActionResult<>(EnumActionResult.PASS, stack);
-        } else {
-            BlockPos blockpos = raytraceresult.getBlockPos();
-
-            if(!world.isBlockModifiable(player, blockpos)) {
-                return new ActionResult<>(EnumActionResult.FAIL, stack);
-            } else {
-                boolean flag1 = world.getBlockState(blockpos).getBlock().isReplaceable(world, blockpos);
-                BlockPos blockpos1 = flag1 && raytraceresult.sideHit == EnumFacing.UP ? blockpos : blockpos.offset(raytraceresult.sideHit);
-
-                if(!player.canPlayerEdit(blockpos1, raytraceresult.sideHit, stack)) {
-                    return new ActionResult<>(EnumActionResult.FAIL, stack);
-                } else if (tryPlaceContainedLiquid(player, world, blockpos1)) {
-                    if(player instanceof EntityPlayerMP) {
-                        CriteriaTriggers.PLACED_BLOCK.trigger((EntityPlayerMP) player, blockpos1, stack);
-                    }
-
-                    player.addStat(Objects.requireNonNull(StatList.getObjectUseStats(this)));
-                    AuraHelper.drainVis(world, blockpos1, 2.0F, false);
-                    return new ActionResult<>(EnumActionResult.SUCCESS, stack);
-                } else
-                    return new ActionResult<>(EnumActionResult.FAIL, stack);
-            }
-        }
+    public static int getQueue(ItemStack stack) {
+        return stack.getTagCompound() != null ? stack.getTagCompound().getInteger("queue") : 0;
     }
 
-    @Override
-    public boolean tryPlaceContainedLiquid(@Nullable EntityPlayer player, World world, @NotNull BlockPos pos) {
-        IBlockState iblockstate = world.getBlockState(pos);
-        Material material = iblockstate.getMaterial();
-        boolean flag = !material.isSolid();
-        boolean flag1 = iblockstate.getBlock().isReplaceable(world, pos);
+    public static void setQueue(ItemStack stack, int queue) {
+        stack.setTagInfo("queue", new NBTTagInt(queue));
+    }
 
-        if(!world.isAirBlock(pos) && !flag && !flag1) {
-            return false;
-        } else if (world.provider.doesWaterVaporize()) {
-            world.playSound(player, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
-
-            for(int k = 0; k < 8; ++k) {
-                world.spawnParticle(EnumParticleTypes.SMOKE_LARGE, pos.getX() + Math.random(), pos.getY() + Math.random(), pos.getZ() + Math.random(), 0.0D, 0.0D, 0.0D);
-            }
-        } else {
-            if(!world.isRemote && (flag || flag1) && !material.isLiquid()) {
-                world.destroyBlock(pos, true);
-            }
-
-            for(int k = 0; k < 20; ++k) {
-                FXDispatcher.INSTANCE.jarSplashFx(pos.getX() + 0.5D, pos.getY() + 1.0D, pos.getZ() + 0.5D);
-            }
-
-            SoundEvent soundevent = SoundEvents.ITEM_BUCKET_EMPTY;
-            world.playSound(player, pos, soundevent, SoundCategory.BLOCKS, 1.0F, 1.0F);
-            world.setBlockState(pos, Blocks.FLOWING_WATER.getDefaultState(), 11);
+    public static class EverfullBucketWrapper extends FluidBucketWrapper {
+        public EverfullBucketWrapper(@NotNull ItemStack container) {
+            super(container);
         }
 
-        return true;
+        protected boolean canDispense() {
+            return getQueue(this.container) < MAX_QUEUE;
+        }
+
+        @Override
+        public boolean canFillFluidType(FluidStack fluidStack) {
+            return false;
+        }
+
+        @Override
+        public @Nullable FluidStack getFluid() {
+            return this.canDispense() ? WATER_STACK : null;
+        }
+
+        @Override
+        protected void setFluid(@Nullable FluidStack fluidStack) {}
+
+        @Override
+        public int fill(FluidStack resource, boolean doFill) {
+            return 0;
+        }
+
+        @Override
+        public @Nullable FluidStack drain(FluidStack resource, boolean doDrain) {
+            if(container.getCount() != 1 || resource == null || !canDispense()) {
+                return null;
+            } else {
+                FluidStack fluid = this.getFluid();
+                if (fluid != null && fluid.isFluidEqual(resource)) {
+                    if(doDrain) {
+                        setQueue(this.container, getQueue(this.container)  + 1);
+                    }
+                    return new FluidStack(fluid.getFluid(), Math.min(fluid.amount, resource.amount));
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public @Nullable FluidStack drain(int maxDrain, boolean doDrain) {
+            return this.drain(new FluidStack(FluidRegistry.WATER, Math.min(Fluid.BUCKET_VOLUME, maxDrain)), doDrain);
+        }
     }
 }
