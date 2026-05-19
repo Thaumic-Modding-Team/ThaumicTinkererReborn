00305f54c957d459075ebd2f47d616fc354852e6
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-05-19 01:26:36 -0500

    Finish up all missing research

--- src/main/java/mod/emt/thaumictinkerer/registry/CreativeTabsTT.java
+++ src/main/java/mod/emt/thaumictinkerer/registry/CreativeTabsTT.java
@@ -5,7 +5,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.NotNull;
-import thaumcraft.api.items.ItemsTC;
 
 public class CreativeTabsTT extends CreativeTabs {
     public CreativeTabsTT(int length, String name) {
@@ -16,6 +15,6 @@ public class CreativeTabsTT extends CreativeTabs {
     @SideOnly(Side.CLIENT)
     @Override
     public @NotNull ItemStack createIcon() {
-        return new ItemStack(ItemsTC.casterBasic);
+        return new ItemStack(ModItemsTT.ENDER_MIRROR);
     }
 }
